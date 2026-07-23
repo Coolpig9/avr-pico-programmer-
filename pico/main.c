dc5c0809b08df3aa6fc638905c595e4291ea32b9
Coolpig_9 <dylan.e.yates@gmail.com>
2026-07-23 17:53:05 -0600

    devided main.c into helper functions, and improved and made settings to retain prevouse functions.

--- pico/main.c
+++ pico/main.c
@@ -4,66 +4,41 @@
 #include <hardware/spi.h>
 #include "avrprog.h"
 #include "embededFirm.h"
+#include "main.h"
+
+#define startButtonPin 0
 
 uint8_t program_code[2000] = {};
 char input[10] = {0};
+bool verify = false;
+bool embededFirm = true;
 
 int main() {
+
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_init(startButtonPin);
+    gpio_set_dir(startButtonPin, GPIO_IN);
     stdio_init_all();
     sleep_ms(1000);
 
-    while (true) {
+    while(true){
+        gpio_put(PICO_DEFAULT_LED_PIN,0);
+        sleep_ms(500);
+        enterStandby();
         int code_c = 0;
-        int input_c = 0;
-        /*
-        while (true) { // read rom
-            char c = getchar();
-
-            // Tells the automatic programmer the programmer is ready.
-            if (c == '?') {
-                printf("READY");
-            }
-
-            if (!(c == 13 || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || c == ' ')) {
-                continue;
-            }
-
-            if (c == 13) {
-                input[input_c] = 0;
-                break;
-            }
-
-            input[input_c] = c;
-
-            if (input[input_c] == ' ') {
-                input[input_c] = 0;
-
-                // Readback the byte.
-                printf(input);
-
-                // Convert from hex to byte.
-                program_code[code_c] = strtol(input, NULL, 16);
-
-                input_c = 0;
-                code_c++;
-            }
-
-            else input_c++;
+        if(!embededFirm){
+            printf("Using Firmware From external Program");
+            code_c = getFirmFromProg();
+            printf("Program length: %d / first & last byte: %hhu %hhu\n", code_c, program_code[0], program_code[code_c - 1]);
+        }else{
+            printf("Using Embeded Firmware To Flash");
+            printf("Program length: %d / first & last byte: %hhu %hhu\n", eByteAmt, eFirm[0], eFirm[eByteAmt - 1]);
         }
-        */
-        for(int i=0;i<2000;i++){
-            program_code[i] = eFirm[i];
-        }
-        code_c = eByteAmt;
-        printf("%i",eByteAmt);
-        int l = sizeof(program_code) / sizeof(program_code[0]);
-
-        printf("Program length: %d / first & last byte: %hhu %hhu\n", code_c, program_code[0], program_code[code_c - 1]);
-
-        //return 1;
+        
         if ((code_c % 2) != 0) {
             printf("program bytes are not a multiple of 2!\n");
-            continue;
+            return 1;
         }
 
         avr_spi_init();
@@ -71,64 +46,143 @@ int main() {
 
         if (!avr_enter_programming_mode()) {
             printf("failed to enter programming mode\n");
-            return 0;
+            return 1;
         }
 
         printf("Entered programming mode\n");
 
         avr_erase_memory();
-
         printf("Erased program memory successfully for programming\n");
+
+        if(embededFirm){
+            flashProgAndVerify(eFirm,eByteAmt,verify);
+        }else{
+            flashProgAndVerify(program_code,code_c,verify);
+        }
+
+
+        printf("Going back to standby mode for future flashes..\n");
+        printf("FINISH");
+    }
+    
+
+    return 0;
+}
+
+void enterStandby(){
+    gpio_put(PICO_DEFAULT_LED_PIN,1);
+    bool buttonState = true;
         
-        // full pages
-        for (int i = 0; i < code_c / 64; i++) {
-            printf("Flashing page %d..\n", i);
+    gpio_pull_up(startButtonPin);
+    while( buttonState == true){
+        buttonState = gpio_get(startButtonPin);
+        sleep_ms(10);
+    }
+    gpio_put(PICO_DEFAULT_LED_PIN,0);
+    while( buttonState == false){
+        buttonState = gpio_get(startButtonPin);
+        sleep_ms(10);
+    }
+    gpio_put(PICO_DEFAULT_LED_PIN,1);
+    bool flashFinished = false;
+}
 
-            int page_offset = i * 64;
+int getFirmFromProg(){
+    int len = 0;
+    int input_c = 0;
+    while (true) { // read rom
+        char c = getchar();
 
-            for (int j = 0; j < 32; j++) {
-                int word_index = (j * 2) + page_offset;
-                
-                uint8_t high_byte = program_code[word_index + 1];
-                uint8_t low_byte = program_code[word_index];
+        // Tells the automatic programmer the programmer is ready.
+        if (c == '?') {
+            printf("READY");
+        }
 
-                avr_write_temporary_buffer(j, low_byte, high_byte);
-            }
+        if (!(c == 13 || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || c == ' ')) {
+            continue;
+        }
 
-            avr_flash_program_memory(i * 32);
+        if (c == 13) {
+            input[input_c] = 0;
+            break;
+        }
 
-            //printf("Verifying flash..\n");
-            //printf("page off: %i\n",page_offset);
-            //printf("prog : %u\n",page_offset);
-            /*
-            if (skipVerifcation || !avr_verify_program_memory_page(i * 32, (uint16_t *)program_code+page_offset, 32)) {
+        input[input_c] = c;
+
+        if (input[input_c] == ' ') {
+            input[input_c] = 0;
+
+            // Readback the byte.
+            printf("%s",input);
+
+            // Convert from hex to byte.
+            program_code[len] = strtol(input, NULL, 16);
+
+            input_c = 0;
+            len++;
+        }
+
+        else input_c++;
+    }
+    return len;
+}
+int flashDelay = 0;
+bool flashProgAndVerify(uint8_t prog[2000],int len, bool doVerify){
+    //full page
+    for (int i = 0; i < len / 64; i++) {
+        printf("Flashing page %d..\n", i);
+
+        int page_offset = i * 64;
+
+        for (int j = 0; j < 32; j++) {
+            sleep_ms(flashDelay/2);
+            gpio_put(PICO_DEFAULT_LED_PIN,0);
+            int word_index = (j * 2) + page_offset;
+            
+            uint8_t high_byte = prog[word_index + 1];
+            uint8_t low_byte = prog[word_index];
+
+            avr_write_temporary_buffer(j, low_byte, high_byte);
+            sleep_ms(flashDelay/2);
+            gpio_put(PICO_DEFAULT_LED_PIN,1);
+        }
+
+        avr_flash_program_memory(i * 32);
+
+        if(doVerify){
+            printf("Verifying flash..\n");
+            if (!avr_verify_program_memory_page(i * 32, (uint16_t *)prog+page_offset, 32)) {
                 printf("Verification failed! Page has not been flashed correctly..\n");
                 sleep_ms(100);
                 return 0;
             } else printf("Page flash successfully verified!\n");
-             */
         }
+    }
+    int remaining_bytes = len % 64;
+    int bytes_offset = len - remaining_bytes;
 
-        int remaining_bytes = code_c % 64;
-        int bytes_offset = code_c - remaining_bytes;
+    // partial page
+    printf("Flashing partial page..\n");
 
-        // partial page
-        printf("Flashing partial page..\n");
-
-        for (int j = 0; j < remaining_bytes / 2; j++) {
-            int word_index = (j * 2) + bytes_offset;
-            
-            uint8_t high_byte = program_code[word_index + 1];
-            uint8_t low_byte = program_code[word_index];
-
-            avr_write_temporary_buffer(j, low_byte, high_byte);
-        }
-
-        avr_flash_program_memory(bytes_offset / 2);
-        /*
-        printf("Verifying flash..\n");
+    for (int j = 0; j < remaining_bytes / 2; j++) {
+        sleep_ms(flashDelay/2);
+        gpio_put(PICO_DEFAULT_LED_PIN,0);
+        int word_index = (j * 2) + bytes_offset;
         
-        if ( || !avr_verify_program_memory_page(bytes_offset / 2, (uint16_t *) program_code + bytes_offset, remaining_bytes / 2)) {
+        uint8_t high_byte = prog[word_index + 1];
+        uint8_t low_byte = prog[word_index];
+
+        avr_write_temporary_buffer(j, low_byte, high_byte);
+        sleep_ms(flashDelay/2);
+        gpio_put(PICO_DEFAULT_LED_PIN,1);
+    }
+    avr_flash_program_memory(bytes_offset / 2);
+    
+    
+    
+    if(doVerify){
+        printf("Verifying flash..\n");
+        if (!avr_verify_program_memory_page(bytes_offset / 2, (uint16_t *) prog + bytes_offset, remaining_bytes / 2)) {
             printf("Verification failed! Page has not been flashed correctly..\n");
             sleep_ms(100);
             return 0;
@@ -136,15 +190,12 @@ int main() {
 
         printf("Verifying overall page flashes..\n");
 
-        if (!avr_verify_program_memory_page(0, (uint16_t *) program_code, code_c / 2)) {
+        if (!avr_verify_program_memory_page(0, (uint16_t *) prog, len / 2)) {
             printf("Overall page verification failed! Pages have not been flashed correctly..\n");
             sleep_ms(100);
             return 0;
         } else printf("Overall page flash verification successful! Firmware has been flashed correctly\n");
-        */
-        printf("Going back to standby mode for future flashes..\n");
-        printf("FINISH");
+        
     }
-
-    return 0;
+    return 1;
 }
---
+++ pico/main.h
@@ -0,0 +1,3 @@
+void enterStandby();
+bool flashProgAndVerify(uint8_t prog[2000], int len, bool doVerify);
+int getFirmFromProg();
